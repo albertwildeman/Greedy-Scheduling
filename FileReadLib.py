Code complete but yields wrong result.

FileReadLib.py
@@ -1,5 +1,7 @@
 import os
 import numpy as np
+
+
 def get_array(filename):
 
     npy_version_exists = os.path.isfile(filename + ".npy")
@@ -16,8 +18,7 @@ def txt_to_npy(filename):
 
     raw_lines = [x[:-1].split(" ") for x in file_array.readlines()]
 
-    nJobs = int(raw_lines[0][0])
-    jobs = np.array([[int(x), int(y)] for x, y in raw_lines[1:]])
+    jobs = np.array([(int(x),int(y)) for x, y in raw_lines[1:]])
 
     file_array.close()
     np.save(filename, jobs)

GreedySchedulerLib.py
@@ -7,13 +7,6 @@ def greedy_scheduler(jobs, method):
     if jobs.shape[1] != 2:
         raise(NameError, "Incorrect number of columns in jobs argument")
 
-    # Initialize the job schedule to be returned
-    schedule = []
-
-
-    print("debug version little jobs")
-    jobs = jobs[:10, ]
-
     if method == "difference":
         # score of each job is its weight minus length
         scores = jobs[:, 0] - jobs[:, 1]
@@ -24,8 +17,8 @@ def greedy_scheduler(jobs, method):
         # method argument is not defined
         raise (NameError, "Invalid method argument")
 
-    job_order = np.lexsort(jobs[:, 0].transpose(), scores)
+    job_order = np.lexsort((jobs[:, 0], scores))[::-1]
+    completion_times = np.cumsum(jobs[job_order,1])
+    cost = np.sum(completion_times * jobs[:,0])
 
-
-
-    return schedule
+    return cost